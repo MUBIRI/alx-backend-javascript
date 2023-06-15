86747ecf3894809f6b5b0c61fdc04c932a0db1f9
MUBIRI <mubirirobberts@gmail.com>
2023-06-15 22:06:47 +0000

    Debug fix

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,9 @@
-function displayMessaage(msg) {
-	console.log(msg);
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
